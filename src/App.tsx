a67642ec8c8a3d6162721ae5f8a74d545225da44
Thenu Kaluarachchi <93025713+thenu-k@users.noreply.github.com>
2022-08-03 23:16:31 +0530

    Added CSR, updated build & dependancies (#9)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
+import { FC } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./Layout/Layout";
+import Home from "./Pages/Home/Home";
 
 
-function App() {
-  return (
-    <div className="App">
-
-    </div>
+const App:FC = () => {
+  return ( 
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout/>}>
+            <Route index element={<Home/>}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/Common/Nav/Nav.tsx
@@ -0,0 +1,12 @@
+import * as S from './Nav.styled'
+
+const Nav = () => {
+  return (
+    <>
+        <S.Header>
+        </S.Header>
+    </>
+  )
+}
+
+export default Nav
