06cd0aace90e8f134a0eaefaa3a0adc216db0f05
Ccastillo06 <ccastillon92@gmail.com>
2022-08-03 20:42:47 +0200

    feat: build with vite and fix deployment

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,14 +1,15 @@
-import * as S from './Home.styled'
-import landing from './../../Assets/landing.png'
+import * as S from './Home.styled';
+import landing from '../../Assets/landing.png';
 
 const Home = () => {
   return (
     <>
-      <S.ImageContainer className='center'>
+      <S.ImageContainer className="center">
+        <h1>Page deployed!</h1>
         <img src={landing} alt="" />
       </S.ImageContainer>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
