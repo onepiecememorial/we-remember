0f5b9d54b1e64b3b0331b868bd5507f922739677
Thenu Kaluarachchi <thenujaya123@gmail.com>
2022-08-03 19:56:30 +0530

    Added styled-components

---
+++ src/Common/Nav/Nav.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import * as S from './Nav.styled'
+
+const Nav = () => {
+  return (
+    <>
+        <S.Header>
+            Nav
+        </S.Header>
+    </>
+  )
+}
+
+export default Nav
--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -1,11 +1,11 @@
 import { FC } from "react"
 import { Outlet } from "react-router"
-
+import Nav from "../Common/Nav/Nav"
 
 const Layout:FC = () => {
   return (
     <div>
-        Nav
+        <Nav/>
         <Outlet/>
     </div>
   )
