41c4b7a3574f82c6e8b1f8851d6315f192bf4451
Ccastillo06 <ccastillon92@gmail.com>
2022-08-04 21:44:32 +0200

    feat: crete script to parse CSV into JSON

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,7 +1,10 @@
 import * as S from './Home.styled';
 import landing from '../../Assets/landing.png';
+import lostNakamas from '../../lost-nakamas.json';
 
 const Home = () => {
+  console.log('Lost nakamas data:', lostNakamas);
+
   return (
     <>
       <S.ImageContainer>
