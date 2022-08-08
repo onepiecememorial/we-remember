f3293decdd99ba8638ba1be0ea62ef894eebef91
toyboatt <alixlol24@gmail.com>
2022-08-08 11:27:55 -0700

    added language detection support for spansih and english

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
+import './i18n.js';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -10,8 +11,10 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
+    <Suspense fallback={<div>Loading...</div>}>
     <HashRouter>
       <App />
     </HashRouter>
+    </Suspense>
   </React.StrictMode>,
 );
