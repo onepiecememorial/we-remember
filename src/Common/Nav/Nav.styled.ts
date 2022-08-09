d68b6a55bb702db08dc59b730ee6755af4488511
Jehan <jehanke8@gmail.com>
2022-08-09 14:12:11 +0530

    modified appbar appearance

--- src/Common/Nav/Nav.styled.ts
+++ src/Common/Nav/Nav.styled.ts
@@ -1,5 +1,16 @@
 import styled from "styled-components"
 
-export const Header = styled.header`
-    
+export const Header = styled.header` 
+    background-color: #272727;
+    display: flex;
+    >div.link-container {
+        padding: 0.938rem;
+        font-size: 1.125rem;
+        margin-top: 0.688rem;
+    }
+    >img.gomu-gomu {
+        height: 50px;
+        width: 50px;
+        padding: 10px;
+    }
 `
